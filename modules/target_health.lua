6fc5d59d174e351e215f3b3350d1bdabe29cb554
stako <stako@users.noreply.github.com>
2024-09-29 17:34:23 -0700

    fix: don't show 0 or 100% health text

--- modules/target_health.lua
+++ modules/target_health.lua
@@ -9,9 +9,12 @@ function module:ADDON_LOADED(name)
   hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", function(statusFrame, textString, value, valueMin, valueMax)
     if statusFrame ~= TargetFrameHealthBar then return end
     if textString:IsShown() and textString:GetText() then return end
-    if value == valueMax then return end
+    if value == valueMax or value == valueMin then return end
+
+    local valueDisplay = math.ceil((value / valueMax) * 100)
+    if valueDisplay > 99 then return end
 
     textString:Show()
-    textString:SetText(math.ceil((value / valueMax) * 100) .. "%")
+    textString:SetText(valueDisplay .. "%")
   end)
 end
