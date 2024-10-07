ee606ab61b0318db87db58c82d36a15911e42cc2
stako <stako@users.noreply.github.com>
2024-10-07 13:58:30 -0700

    move totem text back to bottom

--- modules/totems.lua
+++ modules/totems.lua
@@ -22,10 +22,10 @@ function module:ADDON_LOADED(name)
     TotemFrameTotem4Duration,
   }
 
-  for index, timer in ipairs(timers) do
-    timer:ClearAllPoints()
-    timer:SetPoint("BOTTOM", timer:GetParent(), "TOP", 0, 0)
-  end
+  -- for index, timer in ipairs(timers) do
+  --   timer:ClearAllPoints()
+  --   timer:SetPoint("BOTTOM", timer:GetParent(), "TOP", 0, 0)
+  -- end
 
   -- TotemFrame:ClearAllPoints()
   -- TotemFrame:SetPoint("BOTTOMLEFT", PlayerFrame, "TOPLEFT", 96, -38)
