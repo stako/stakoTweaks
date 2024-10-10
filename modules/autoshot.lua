20f872df25f7e2a5f8b26bdfd4b1d908134e583f
stako <stako@users.noreply.github.com>
2024-10-10 00:55:51 -0700

    refactor: improve autoshot display logic

--- modules/autoshot.lua
+++ modules/autoshot.lua
@@ -2,27 +2,48 @@ local addonName, addon = ...
 if addon.playerClass ~= "HUNTER" then return end
 local module = addon:NewModule()
 
+local autoShotDisabled = true
+local inCombat = false
+local inZone = false
+
 local text = UIParent:CreateFontString(nil, "OVERLAY", "SystemFont_Huge1_Outline")
 text:SetPoint("CENTER", 0, -55)
 text:SetText("|T132369:14|t Auto Shot Disabled |T132369:14|t")
 text:Hide()
 module.text = text
 
+addon:RegisterEvent("PLAYER_ENTERING_WORLD")
 addon:RegisterEvent("START_AUTOREPEAT_SPELL")
 addon:RegisterEvent("PLAYER_REGEN_ENABLED")
+addon:RegisterEvent("PLAYER_REGEN_DISABLED")
 addon:RegisterEvent("STOP_AUTOREPEAT_SPELL")
 
+function module:PLAYER_ENTERING_WORLD()
+  local _, instanceType = IsInInstance()
+  inZone = (instanceType == "party" or instanceType == "raid")
+  self:UpdateVisibility()
+end
+
 function module:START_AUTOREPEAT_SPELL()
-  self.text:Hide()
+  autoShotDisabled = false
+  self:UpdateVisibility()
 end
 
 function module:PLAYER_REGEN_ENABLED()
-  self.text:Hide()
+  inCombat = false
+  self:UpdateVisibility()
+end
+
+function module:PLAYER_REGEN_DISABLED()
+  inCombat = true
+  self:UpdateVisibility()
 end
 
 function module:STOP_AUTOREPEAT_SPELL()
-  local _, instanceType = IsInInstance()
-  if instanceType ~= "party" and instanceType ~= "raid" then return end
+  autoShotDisabled = true
+  self:UpdateVisibility()
+end
 
-  self.text:SetShown(InCombatLockdown())
+function module:UpdateVisibility()
+  self.text:SetShown(autoShotDisabled and inCombat and inZone)
 end
