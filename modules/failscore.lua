5d68e502e8bf10598f3ca0db7f12fc05315b0d9f
stako <stako@users.noreply.github.com>
2024-11-06 22:54:49 -0800

    feat: add failscore rankings

--- modules/failscore.lua
+++ modules/failscore.lua
@@ -14,7 +14,6 @@ function module:ADDON_LOADED(name)
   if name ~= addonName then return end
 
   self:BuildDB()
-  self:UpdateGuildDB()
 end
 
 function module:GUILD_PARTY_STATE_UPDATED(isGuildGroup)
@@ -31,16 +30,28 @@ function module:CHAT_MSG_WHISPER(...)
   local msg, _, _, _, name, _, _, _, _, _, _, guid = ...
   if string.lower(msg) ~= "failscore" or not guid then return end
 
+  self:UpdateGuildDB()
+  self:UpdateRankings()
+
   local player = self.guilddb[guid]
   local leader = self.guilddb.leader
+
+  if not leader.name then return end
+
   local pluralizeDeath = leader.score > 1 and " deaths" or " death"
-  local message = " :: " .. leader.name .. " leads with " .. leader.score .. pluralizeDeath
+  local leaderMessage = " || Leader: " .. leader.name .. " (" .. leader.score .. pluralizeDeath .. ")"
+  local message
 
   if player then
-    pluralizeDeath = player.score > 1 and " deaths" or " death"
-    message = "You have " .. player.score .. pluralizeDeath .. message
+    local rank = self.guilddb.rankings[guid]
+    local deathsMessage = "Deaths: " .. player.score
+    local rankMessage = " || Rank: " .. rank
+    message = deathsMessage .. rankMessage
+    if rank > 1 then
+      message = message .. leaderMessage
+    end
   else
-    message = "You have no deaths on record" .. message
+    message = "No deaths on record" .. leaderMessage
   end
 
   SendChatMessage(message, "WHISPER", nil, name)
@@ -50,6 +61,7 @@ function module:COMBAT_LOG_EVENT_UNFILTERED()
   local _, event, _, _, _, _, _, destGuid, destName, destFlags = CombatLogGetCurrentEventInfo()
 
   if event == "UNIT_DIED" and hasFlag(destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) then
+    self:UpdateGuildDB()
     self:IncreaseScore(destGuid, destName)
     self:UpdateLeader(destGuid)
   end
@@ -57,13 +69,15 @@ end
 
 function module:BuildDB()
   local db = ns:GetDB()
-  db.failscore = db.failscore or {}
+  db.failscore = db.failscore or { lastDeath = 0, lastRanking = 0 }
   self.db = db.failscore
 end
 
 function module:UpdateGuildDB()
+  if self.guilddb then return end
+
   local guild = GetGuildInfo("player")
-  self.db[guild] = self.db[guild] or { leader = {score = 0}}
+  self.db[guild] = self.db[guild] or { rankings = {}, leader = {score = 0}}
   self.guilddb = self.db[guild]
 end
 
@@ -75,6 +89,8 @@ function module:IncreaseScore(guid, name)
     playerdb.name = name
     playerdb.score = playerdb.score + 1
   end
+
+  self.db.lastDeath = GetTime()
 end
 
 function module:UpdateLeader(guid)
@@ -97,4 +113,35 @@ function module:AnnounceNewLeader()
   local message = leader.name .. " has taken the lead with " .. leader.score .. death .. "! Whisper me 'failscore' for your score."
 
   SendChatMessage(message, "RAID")
+end
+
+function module:UpdateRankings()
+  if self.db.lastDeath < self.db.lastRanking then return end
+
+  local sortedList = {}
+  local rankings = self.guilddb.rankings
+
+  for guid, data in pairs(self.guilddb) do
+    if guid ~= "leader" and guid ~= "rankings" then
+      tinsert(sortedList, {guid = guid, score = data.score})
+    end
+  end
+
+  table.sort(sortedList, function(a, b)
+    return a.score > b.score
+  end)
+
+  local currentRank = 1
+  for i = 1, #sortedList do
+    local player = sortedList[i]
+
+    if i > 1 and player.score < sortedList[i-1].score then
+      currentRank = currentRank + 1
+    end
+
+    rankings[player.guid] = currentRank
+  end
+
+
+  self.db.lastRanking = GetTime()
 end
